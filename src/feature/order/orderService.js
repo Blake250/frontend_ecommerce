f41c591e96f0b0b223919a18dcc1196469016d73
Christian <89995961+Blake250@users.noreply.github.com>
2024-05-20 05:50:01 +0100

    Update orderService.js

--- src/feature/order/orderService.js
+++ src/feature/order/orderService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 //const BACKEND_URL = process.env.REACT_APP_BACKEND_URL 
-const BACKEND_URL = `https://api-shopito-cgp4.onrender.com`
+const BACKEND_URL = "https://api-shopito-cgp4.onrender.com"
   export const API_URL = `${BACKEND_URL}/api/order/`
 
 
